dff2eea6143ca07990e6ebcbfa3193113c0adc85
bradykearley <bradykearley84@gmail.com>
2024-10-19 12:17:44 -0400

    Push To Main

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,16 @@
 import { Central as Layout } from "@/layouts";
 import "./NotFound.style.scss";
-
+import homer404 from "@/Gifs/homer404.gif"
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <div className="not-found-container">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-message">
+          Oops! The page you're looking for doesn't exist.
+        </p>
+        <img src={homer404} alt="404 GIF" className="not-found-gif" />
+      </div>
     </Layout>
   );
 }
